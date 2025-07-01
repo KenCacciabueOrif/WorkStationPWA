899f678661e4c4c805a0abe49ada730595345adb
Ken Cacciabue <ken.cacciabue@formation.orif.ch>
2025-07-01 11:43:01 +0200

    Calendar

--- workstation-app/app/page.tsx
+++ workstation-app/app/page.tsx
@@ -1,12 +1,14 @@
 import Head from "next/head";
-import SWRegister from "@/components/SWRegister";
+import SWRegister from "@/components/clientComposants/SWRegister";
+import Calendar from "@/components/clientComposants/calendar";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen flex flex-col p-4">
       <Head>
         <link rel="icon" href="/apple-touch-icon.png"/>
       </Head>
       <SWRegister/>
+      <Calendar/>
     </div>
   );
 }
